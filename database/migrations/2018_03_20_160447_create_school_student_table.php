6985f7ca6d212462aabfa91bb9184a990662820e
ChristianMitrofan <32639939+ChristianMitrofan@users.noreply.github.com>
2018-03-20 19:23:03 +0000

    Update 2018_03_20_160447_create_school_student_table.php

--- database/migrations/2018_03_20_160447_create_school_student_table.php
+++ database/migrations/2018_03_20_160447_create_school_student_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStudentSchoolTable extends Migration
+class CreateSchoolStudentTable extends Migration
 {
     /**
      * Run the migrations.
